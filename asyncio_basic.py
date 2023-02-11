bb1918aee0622509f5344f9771119446bd0b39cd
dinghao <dinghao0210@163.com>
2023-02-11 16:53:07 +0800

    v1

---
+++ asyncio_basic.py
@@ -0,0 +1,17 @@
+import asyncio
+
+
+async def other():
+    print("start")
+    await asyncio.sleep(2)
+    print("end")
+    return "返回值"
+
+
+async def func():
+    print("执行协程函数内部代码")
+    response = await other()
+    print(response)
+
+
+asyncio.run(func())
